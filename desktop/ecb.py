d336042da6de498d5bcee44d1d67cf09a4d2d389
Urmi Dedhia <dedhia.urmi20@gmail.com>
2023-05-17 08:28:05 +0530

    .

--- desktop/ecb.py
+++ desktop/ecb.py
@@ -52,3 +52,69 @@ for elem in blocks:
     p = Process(elem, 3)
     p.start()
     p.join()
+
+# option
+def text_to_binary(text):
+    return ''.join(format(ord(c), '08b') for c in text)
+
+def binary_to_text(binary):
+    return ''.join(chr(int(binary[i:i+8], 2)) for i in range(0, len(binary), 8))
+
+def ecb_encrypt(plaintext, key, t, mode):
+    binary_plaintext = text_to_binary(plaintext)
+
+    padding_length = 128 - len(binary_plaintext) % 128
+    binary_plaintext += '0' * padding_length
+
+    blocks = [binary_plaintext[i:i+128] for i in range(0, len(binary_plaintext), 128)]
+
+    ciphertext_blocks = []
+    for block in blocks:
+        if mode == 'left':
+            block = block[t:] + block[:t] 
+        elif mode == 'right':
+            block = block[-t:] + block[:-t]
+
+        
+
+        ciphertext_blocks.append(block)
+
+    ciphertext_binary = ''.join(ciphertext_blocks)
+    ciphertext = binary_to_text(ciphertext_binary)
+
+    return ciphertext
+
+plaintext = "This is a sample plaintext to be encrypted using ECB mode."
+key = "secretkey"
+t = 3
+mode = 'left'
+
+ciphertext = ecb_encrypt(plaintext, key, t, mode)
+
+print("Plaintext: ", plaintext)
+print("Ciphertext:", ciphertext)
+
+def ecb_decrypt(ciphertext, key, t, mode):
+    binary_ciphertext = text_to_binary(ciphertext)
+
+    blocks = [binary_ciphertext[i:i+128] for i in range(0, len(binary_ciphertext), 128)]
+
+    plaintext_blocks = []
+    for block in blocks:
+        decrypted_block = block
+        if mode == 'left':
+            decrypted_block = decrypted_block[-t:] + decrypted_block[:-t]  
+        elif mode == 'right':
+            decrypted_block = decrypted_block[t:] + decrypted_block[:t]  
+
+        plaintext_blocks.append(decrypted_block)
+
+    plaintext_binary = ''.join(plaintext_blocks)
+    plaintext = binary_to_text(plaintext_binary)
+
+    return plaintext
+
+decrypted_text = ecb_decrypt(ciphertext, key, t, mode)
+
+print("Ciphertext:", ciphertext)
+print("Decrypted text:", decrypted_text)
